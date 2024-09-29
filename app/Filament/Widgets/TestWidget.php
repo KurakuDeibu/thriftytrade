7e4d073df92d878c3d409b749b6e6c6070c388c8
DAVIX <davixdelapena30@gmail.com>
2024-09-29 17:25:28 +0800

    -updated user widgets with stats and icons.

--- app/Filament/Widgets/TestWidget.php
+++ app/Filament/Widgets/TestWidget.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\User;
+use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,13 +11,26 @@ class TestWidget extends BaseWidget
 {
     protected function getStats(): array
     {
+        $newUsersCount = User::where('created_at', '>=', now()->subWeek())->count();
+        $verifiedUsersCount = User::where('email_verified_at', '!=', null)->count();
+        $totalUsersCount = User::count();
+
         return [
-            Stat::make('New Users', User::where('created_at', '>=', now()->subWeek())->count())
-                ->icon('heroicon-o-user'),
-            Stat::make('Verified Users', User::where('email_verified_at', '!=', null)->count())
-                ->icon('heroicon-o-check-badge'),
-            Stat::make('Total Users', User::count())
-                ->icon('heroicon-o-users'),
+            Stat::make('New Users', $newUsersCount)
+                ->description('New users that have joined')
+                ->descriptionIcon('heroicon-o-user', IconPosition::Before)
+                ->chart([0,$newUsersCount])
+                ->color('info'),
+            Stat::make('Verified Users', $verifiedUsersCount)
+                ->description('Users that are verified')
+                ->descriptionIcon('heroicon-o-check-badge', IconPosition::Before)
+                ->chart([0, $verifiedUsersCount])
+                ->color('primary'),
+            Stat::make('Total Users', $totalUsersCount)
+                ->description('All registered users')
+                ->descriptionIcon('heroicon-o-users', IconPosition::Before)
+                ->chart([0, $totalUsersCount])
+                ->color('primary'),
         ];
     }
 }
