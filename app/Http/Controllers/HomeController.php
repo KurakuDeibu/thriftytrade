dca024bc1349c519f4ce7743b1b5e72e19769714
DAVIX <davixdelapena30@gmail.com>
2024-09-30 13:02:22 +0800

    -added caching to HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -13,9 +15,15 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        //Added caching to show products
+        $featuredProducts = Cache::remember('featuredProducts', Carbon::now()->addHour(5), function () {
+        return Product::featured()->latest('created_at')->take(6)->get();
+        });
+
+        // home.blade.php / landing page
         return view('home', [
-            'featuredProducts' => Product::featured()->latest('created_at')->take(6)->get()
-        ]); // home.blade.php / landing page
+            'featuredProducts' => $featuredProducts
+        ]);
 
     }
 }
