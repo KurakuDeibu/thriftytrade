95be3f3ac3514af31ab13a3cfcc38f0af40a66f0
DAVIX <158012127+KurakuDeibu@users.noreply.github.com>
2024-12-19 19:18:56 +0800

    Update build-app.sh

--- build-app.sh
+++ build-app.sh
@@ -9,14 +9,14 @@ composer install
 # Clear cache
 php artisan optimize:clear
 
-
 # Cache the various components of the Laravel application
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
-php artisan filament:optimize
-php artisan filament:optimize-clear
 php artisan view:cache
 php artisan optimize
+php artisan icons:cache
+php artisan filament:cache-components
+php artisan filament:assets
 
 php artisan storage:link
